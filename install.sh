c4500781382666918505d2d7cd62ffe56639d4d6
David Daeschler <david.daeschler@gmail.com>
2016-07-25 16:51:43 -0500

    Automatically install mysql connector on cent/el7

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# NOTE: Requires mysql connector for python
+# On centos: https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.3-1.el7.x86_64.rpm
+
+if [[ $(uname -a) == *".el7.x86_64"* ]]; then
+    rpm -ivh https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.3-1.el7.x86_64.rpm
+fi
+
 mkdir -p /opt/slidewave
 cp -f mysql-table-swapcopy.py /opt/slidewave
 chmod +x /opt/slidewave/mysql-table-swapcopy.py
